0cb50d3eb53695e24899f099a163cab1f533f042
Noperkot <92878142+Noperkot@users.noreply.github.com>
2022-04-17 11:59:15 +0300

    initial

--- src/dialogs.h
+++ src/dialogs.h
@@ -6,5 +6,6 @@ BOOL CALLBACK OptDlgProc ( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
 BOOL CALLBACK AboutDlgProc ( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
 int fontSize( HWND hWnd, int size );
 void center_dlg( HWND hWnd );
+void OnClick();
 
 #endif // DIALOGS_H
