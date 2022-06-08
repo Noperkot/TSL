8dc9639350ca8abe7f594fd7e3b4b953fc6c86c7
Noperkot <92878142+Noperkot@users.noreply.github.com>
2022-06-08 14:21:44 +0300

    TS download/remove scripts

--- NSIS/TorrServers/downloadAll.sh
+++ NSIS/TorrServers/downloadAll.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
+
+baseurl="https://github.com/YouROK/TorrServer/releases/download"
+
 function dwnld {
 	ver=`basename "$1"`
 	echo $ver
-	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$ver/TorrServer-windows-amd64.exe" -P "$1"
-	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$ver/TorrServer-windows-386.exe"   -P "$1"
+	wget -q --show-progress -c "$baseurl/$ver/TorrServer-windows-amd64.exe" -P "$1"
+	wget -q --show-progress -c "$baseurl/$ver/TorrServer-windows-386.exe"   -P "$1"
 	echo
 }
 
 if [ -z "$1" ]
-then
-	find `dirname $0` -mindepth 1 -maxdepth 1 -type d | sort -n | while read dir; do dwnld "$dir"; done
-else
-	dwnld "$1"
+	then
+		find `dirname $0` -mindepth 1 -maxdepth 1 -type d | sort -n | while read dir; do dwnld "$dir"; done
+	else
+		dwnld "$1"
 fi
+
+echo -e "Done\n"
+sleep 2
 
 
 
--- NSIS/TorrServers/removeAll.sh
+++ NSIS/TorrServers/removeAll.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 find . -type f -name "TorrServer-windows-*.exe"  -exec rm -f "{}" +
-echo All TorrServer binaries was removed
+echo -e "\nAll TorrServer binaries was removed\n"
+sleep 2
   
