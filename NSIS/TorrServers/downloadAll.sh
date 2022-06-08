5432befdee5d43062ae2fede42c6c8c64cbd7e27
Noperkot <92878142+Noperkot@users.noreply.github.com>
2022-06-08 16:43:43 +0300

    TS download/remove scripts

--- NSIS/TorrServers/downloadAll.sh
+++ NSIS/TorrServers/downloadAll.sh
@@ -3,18 +3,26 @@
 baseurl="https://github.com/YouROK/TorrServer/releases/download"
 
 function dwnld {
+	wget -q --show-progress -c "$1" -P "$2"
+	if [[ $? -ne 0 ]]; then
+		echo -e "\n!!! wget failed !!!\n"
+		exit 1; 
+	fi
+}
+
+function dwnldAll {
 	ver=`basename "$1"`
 	echo $ver
-	wget -q --show-progress -c "$baseurl/$ver/TorrServer-windows-amd64.exe" -P "$1"
-	wget -q --show-progress -c "$baseurl/$ver/TorrServer-windows-386.exe"   -P "$1"
+	dwnld "$baseurl/$ver/TorrServer-windows-amd64.exe" "$1"
+	dwnld "$baseurl/$ver/TorrServer-windows-386.exe"   "$1"	
 	echo
 }
 
 if [ -z "$1" ]
 	then
-		find `dirname $0` -mindepth 1 -maxdepth 1 -type d | sort -n | while read dir; do dwnld "$dir"; done
+		find `dirname $0` -mindepth 1 -maxdepth 1 -type d | sort -n | while read dir; do dwnldAll "$dir"; done || exit 1
 	else
-		dwnld "$1"
+		dwnldAll "$1"
 fi
 
 echo -e "Done\n"
