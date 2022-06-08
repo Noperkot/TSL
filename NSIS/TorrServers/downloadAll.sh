8c6545ae928bed9c530d73b09bbff3a9953add9e
Noperkot <92878142+Noperkot@users.noreply.github.com>
2022-06-08 13:41:20 +0300

    TS download script modified

--- NSIS/TorrServers/downloadAll.sh
+++ NSIS/TorrServers/downloadAll.sh
@@ -1,13 +1,14 @@
 function dwnld {
-	echo $1
-	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$1/TorrServer-windows-amd64.exe" -P "./$1"
-	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$1/TorrServer-windows-386.exe"   -P "./$1"
+	ver=`basename "$1"`
+	echo $ver
+	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$ver/TorrServer-windows-amd64.exe" -P "$1"
+	wget -N -q --show-progress "https://github.com/YouROK/TorrServer/releases/download/$ver/TorrServer-windows-386.exe"   -P "$1"
 	echo
 }
 
 if [ -z "$1" ]
 then
-	find . -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | while read dir; do dwnld "$dir"; done
+	find `dirname $0` -mindepth 1 -maxdepth 1 -type d | sort -n | while read dir; do dwnld "$dir"; done
 else
 	dwnld "$1"
 fi
