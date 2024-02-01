c47f015f8d90e1ef7ad37a61792f1970df3f4d0c
Noperkot <92878142+Noperkot@users.noreply.github.com>
2024-02-01 18:16:41 +0300

    TorrServer Installer has been moved to its own repository

--- NSIS/build_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-# Если собирать под линухом в NSIS 3.04-1 - ругаются четыре движка на вирустотал. На собранные под виндой в NSIS 3.04 - нет(самоподписанные).
-# И медленно очень, под виндой заметно быстрее.
-
-find ./TorrServers -mindepth 1 -maxdepth 1 -type d -printf "-V3 -DTS_VERSION=%f ./offline_Installer.nsi\n" | xargs -L1 makensis 
