8cbbfaf6db54b3f314c8dbed097533a80123d56b
Noperkot <92878142+Noperkot@users.noreply.github.com>
2024-02-15 19:21:48 +0300

    v. 1.8.0

--- src/res/version.h
+++ src/res/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define _FlVERSION_			1,7,2,0
-#define _PrVERSION_			1,7,2,0
-#define _FILEVERSION_		L"1.7.2.0"
-#define _PRODUCTVERSION_	L"1.7.2"
+#define _FlVERSION_			1,8,0,0
+#define _PrVERSION_			1,8,0,0
+#define _FILEVERSION_		L"1.8.0.0"
+#define _PRODUCTVERSION_	L"1.8.0.0"
 #define _PRODUCTNAME_		L"TorrServer Launcher"
 #define _INTERNALNAME_		L"TorrServer Launcher"
 #define _FILEDESCRIPTION_	L"TorrServer Launcher"
